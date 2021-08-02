4898b87f6d38f847fc3a7bc24be56d4296220718
gauravkrv675 <gauravkrv675@gmail.com>
2021-08-02 17:07:50 +0530

    Updates

---
+++ precache-manifest.41493896a4b6e4d511dcc28a3f829212.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d8574f39ea8b0d5d0a0e8cdc0a4648",
+    "url": "/allopa/index.html"
+  },
+  {
+    "revision": "2ac5e7ffc73930e26b60",
+    "url": "/allopa/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "ba338f90458fe8d0e420",
+    "url": "/allopa/static/css/main.7a815947.chunk.css"
+  },
+  {
+    "revision": "2ac5e7ffc73930e26b60",
+    "url": "/allopa/static/js/2.670fdb07.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/allopa/static/js/2.670fdb07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba338f90458fe8d0e420",
+    "url": "/allopa/static/js/main.6adbb286.chunk.js"
+  },
+  {
+    "revision": "e32252cf6da2e1617a33",
+    "url": "/allopa/static/js/runtime-main.a0a6edda.js"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/allopa/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "f3564ebd5f05d3e2d2a8130550f88142",
+    "url": "/allopa/static/media/blockchain.f3564ebd.jpeg"
+  },
+  {
+    "revision": "9c148e13d1f217b3e283e6bd3afb513c",
+    "url": "/allopa/static/media/cybersecurity.9c148e13.jpg"
+  },
+  {
+    "revision": "e4fdfe88042e03d8464be3c1187ebc3b",
+    "url": "/allopa/static/media/hero-img.e4fdfe88.jpg"
+  },
+  {
+    "revision": "6ad60d0ba6a0f726c82d9f14c336ab60",
+    "url": "/allopa/static/media/img2.6ad60d0b.svg"
+  },
+  {
+    "revision": "84a96f105589105fcd377cc1f9928247",
+    "url": "/allopa/static/media/more.84a96f10.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/allopa/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/allopa/static/media/software.43049cae.jpeg"
+  }
+]);
